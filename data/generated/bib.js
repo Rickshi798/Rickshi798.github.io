e22f1155636015c0c29f9a4c6dad7ddc65976aa7
Rickshi798 <133591878+Rickshi798@users.noreply.github.com>
2023-05-14 17:27:33 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,85 @@
+﻿const generatedBibEntries = {
+    "deng1999color": {
+        "author": "Deng, Yining and Manjunath, B Shin and Shin, Hyundoo",
+        "booktitle": "Proceedings. 1999 IEEE Computer Society Conference on Computer Vision and Pattern Recognition (Cat. No PR00149)",
+        "organization": "IEEE",
+        "pages": "446--451",
+        "title": "Color image segmentation",
+        "type": "inproceedings",
+        "volume": "2",
+        "year": "1999"
+    },
+    "gao2022convmae": {
+        "author": "Gao, Peng and Ma, Teli and Li, Hongsheng and Dai, Jifeng and Qiao, Yu",
+        "journal": "arXiv preprint arXiv:2205.03892",
+        "title": "Convmae: Masked convolution meets masked autoencoders",
+        "type": "article",
+        "year": "2022"
+    },
+    "he2023accuracy": {
+        "author": "He, Sheng and Bao, Rina and Li, Jingpeng and Grant, P Ellen and Ou, Yangming",
+        "journal": "arXiv preprint arXiv:2304.09324",
+        "title": "Accuracy of Segment-Anything Model (SAM) in medical image segmentation tasks",
+        "type": "article",
+        "year": "2023"
+    },
+    "he2023u": {
+        "author": "He, Sheng and Bao, Rina and Grant, P Ellen and Ou, Yangming",
+        "journal": "arXiv preprint arXiv:2304.01401",
+        "title": "U-Netmer: U-Net meets Transformer for medical image segmentation",
+        "type": "article",
+        "year": "2023"
+    },
+    "kirillov2023segment": {
+        "author": "Kirillov, Alexander and Mintun, Eric and Ravi, Nikhila and Mao, Hanzi and Rolland, Chloe and Gustafson, Laura and Xiao, Tete and Whitehead, Spencer and Berg, Alexander C and Lo, Wan-Yen and others",
+        "journal": "arXiv preprint arXiv:2304.02643",
+        "title": "Segment anything",
+        "type": "article",
+        "year": "2023"
+    },
+    "ma2023segment": {
+        "author": "Ma, Jun and Wang, Bo",
+        "journal": "arXiv preprint arXiv:2304.12306",
+        "title": "Segment anything in medical images",
+        "type": "article",
+        "year": "2023"
+    },
+    "mata2022medicalseg": {
+        "author": "Mata, Christian and Munuera, Josep and Lalande, Alain and Ochoa-Ruiz, Gilberto and Benitez, Raul",
+        "journal": "Algorithms",
+        "number": "6",
+        "pages": "200",
+        "publisher": "MDPI",
+        "title": "MedicalSeg: A Medical GUI Application for Image Segmentation Management",
+        "type": "article",
+        "volume": "15",
+        "year": "2022"
+    },
+    "minaee2021image": {
+        "author": "Minaee, Shervin and Boykov, Yuri and Porikli, Fatih and Plaza, Antonio and Kehtarnavaz, Nasser and Terzopoulos, Demetri",
+        "journal": "IEEE transactions on pattern analysis and machine intelligence",
+        "number": "7",
+        "pages": "3523--3542",
+        "publisher": "IEEE",
+        "title": "Image segmentation using deep learning: A survey",
+        "type": "article",
+        "volume": "44",
+        "year": "2021"
+    },
+    "razzak2018deep": {
+        "author": "Razzak, Muhammad Imran and Naz, Saeeda and Zaib, Ahmad",
+        "journal": "Classification in BioApps: Automation of Decision Making",
+        "pages": "323--350",
+        "publisher": "Springer",
+        "title": "Deep learning for medical image processing: Overview, challenges and the future",
+        "type": "article",
+        "year": "2018"
+    },
+    "zou2023segment": {
+        "author": "Zou, Xueyan and Yang, Jianwei and Zhang, Hao and Li, Feng and Li, Linjie and Gao, Jianfeng and Lee, Yong Jae",
+        "journal": "arXiv preprint arXiv:2304.06718",
+        "title": "Segment everything everywhere all at once",
+        "type": "article",
+        "year": "2023"
+    }
+};
